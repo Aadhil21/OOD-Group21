f2e5a576a4582d97ce805551d0b35e87a4034bfa
Adeepa Gunathilake <iceblob@proton.me>
2023-11-22 02:49:52 +0530

    set fxml file to load only when needed

--- src/main/java/com/sacms/controllers/ScreenController.java
+++ src/main/java/com/sacms/controllers/ScreenController.java
@@ -1,20 +1,26 @@
 package com.sacms.controllers;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class ScreenController {
-    private static final HashMap<String, Pane> screenMap = new HashMap<>();
-    private static Scene main = null;
+    private static final HashMap<String, URL> screenMap = new HashMap<>();
+    private static Stage main = null;
 
-    public ScreenController(Scene main) {
+    public ScreenController(Stage main) {
         ScreenController.main = main;
     }
 
-    public void addScreen(String name, Pane pane){
-        screenMap.put(name, pane);
+    public void addScreen(String name, URL resource){
+        screenMap.put(name, resource);
     }
 
     protected void removeScreen(String name){
@@ -22,6 +28,12 @@ public class ScreenController {
     }
 
     public static void activate(String name){
-        main.setRoot( screenMap.get(name) );
+        try {
+            Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(screenMap.get(name))));
+            main.setScene(scene);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Program crashed! Cannot open window.").showAndWait();
+            throw new RuntimeException(e); // kills the program
+        }
     }
 }
