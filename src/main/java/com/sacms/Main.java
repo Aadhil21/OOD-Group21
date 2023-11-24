5693d24cf99795819adc3c2ca228487b66a92a07
Adeepa Gunathilake <iceblob@proton.me>
2023-11-24 15:19:00 +0530

    Fix: JavaFX Location not found error on app launch

--- src/main/java/com/sacms/Main.java
+++ src/main/java/com/sacms/Main.java
@@ -15,7 +15,7 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),600,500);
         stage.setTitle("SACMS!");
         stage.setScene(scene);
