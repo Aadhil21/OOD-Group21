fb6429951f61b374851529a958a26b1794e2a61f
Adeepa Gunathilake <iceblob@proton.me>
2023-11-27 02:31:22 +0530

    Simplify imports

--- src/main/java/com/sacms/controllers/AdvisorDashboard.java
+++ src/main/java/com/sacms/controllers/AdvisorDashboard.java
@@ -10,12 +10,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Window;
 
