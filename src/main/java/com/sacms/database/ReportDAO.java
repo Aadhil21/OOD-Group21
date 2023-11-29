2a6fb109ee81857e0bfef4dd0642dab0c3a34fdd
Aadhil21 <aadhil.lk@gmail.com>
2023-11-29 14:29:28 +0530

    added report sql queries

--- src/main/java/com/sacms/database/ReportDAO.java
+++ src/main/java/com/sacms/database/ReportDAO.java
@@ -7,13 +7,13 @@ public class ReportDAO {
     private static final DBManager dbManager = DBManager.getInstance();
 
     public ResultSet GetMembershipReport(String ClubName) throws SQLException {
-        final String getMembers = String.format("SELECT student FROM Members WHERE club = %s", ClubName);
+        final String getMembers = String.format("SELECT S.uid, S.first_name, S.last_name, S.email, S.phone FROM Members M JOIN Student S ON M.student = S.uid WHERE M.club = '%s'", ClubName);
         ResultSet resultSet = dbManager.executeSQLQuery(getMembers).resultSet;
         return resultSet;
     }
 
     public ResultSet GetEvents(String ClubName) throws SQLException {
-        final String getMembers = String.format("SELECT title FROM Events WHERE club = %s", ClubName);
+        final String getMembers = String.format("SELECT title FROM Events WHERE club = '%s'", ClubName);
         ResultSet resultSet = dbManager.executeSQLQuery(getMembers).resultSet;
         return resultSet;
     }
